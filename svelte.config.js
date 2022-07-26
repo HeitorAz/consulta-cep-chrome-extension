f11c31d72c5f17d6ead17e67adac0e291728d5b0
Heitor <tughug11@gmail.com>
2022-07-26 02:00:51 -0300

    Setup Finished

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,19 @@
-import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
+import adapter from 'sveltekit-adapter-chrome-extension';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: [
+		preprocess({
+			postcss: true
+		})
+	],
 
 	kit: {
 		adapter: adapter(),
+		appDir: 'app',
 
 		// Override http methods in the Todo forms
 		methodOverride: {
