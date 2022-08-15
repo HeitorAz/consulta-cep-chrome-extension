f9bfbbe9ed2a34634cc92e0ae9076834efad2448
Heitor <tughug11@gmail.com>
2022-08-15 11:04:23 -0300

    Display default flag
    Displays a default state flag, on the address details page, in case no
    image for that address' state is available.

--- src/lib/widgets/address/AddressInfoItem.svelte
+++ src/lib/widgets/address/AddressInfoItem.svelte
@@ -7,6 +7,10 @@ import { ToastType } from "$src/lib/models/toast";
 
     export let flag: string
 
+    const validFlags = ['AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO'];
+
+    let isFlagValid = flag && validFlags.includes(flag);
+
     function copyValue() {
         navigator.clipboard.writeText(value);
         showNotification(`${label} copiado!`, {type: ToastType.Success});
@@ -20,8 +24,10 @@ import { ToastType } from "$src/lib/models/toast";
         <h1 class="text-xs text-primary-500 font-semibold tracking-wider mb-1">{label}</h1>
         <div class="flex">
             <div class="flex flex-row justify-start items-center w-4/5">
-                {#if flag}
+                {#if flag && isFlagValid}
                     <img src="/images/flags/{flag}.png" width="30" class="mr-1" alt="{label}">
+                {:else if flag && !isFlagValid}
+                    <img src="/images/flags/Unknown.png" width="30" class="mr-1" alt="{label}">
                 {/if}
                 <span class="font-semibold ml-1 text-primary-900">{value}</span>
             </div>
